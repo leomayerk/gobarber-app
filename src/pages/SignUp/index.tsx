37ac7c3b16c014b02e16a59bc60a3beec00d5cbc
leomayerk <leomayerk@hotmail.com>
2020-12-20 12:33:16 -0300

    Navigation configure

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SignIn } from '../SignIn';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => {
+  return <Container />;
+};
+
+export default SignUp;
